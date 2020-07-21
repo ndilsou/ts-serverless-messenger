e1d3e3d6291922605f9dfceaa6afb19b41d90ad5
Ndil-Adjim Sou Ngadoy <ndiladjim.soungadoy@trinitymirror.com>
2020-07-21 20:45:29 +0200

    add entities definitions

---
+++ src/common/entities.ts
@@ -0,0 +1,49 @@
+export interface Conversation {
+  id: string;
+}
+
+export interface User {
+  id: string;
+  createdAt: Date
+  name: string;
+  email: string;
+}
+
+export type Event =
+  | JoinConversationEvent
+  | LeaveConversationEvent  
+  | SendAttachementEvent
+  | SendDirectMessageEvent
+  | SendMessageEvent;
+
+export interface EventBase {
+  action: string;
+  id?: string;
+  timestamp: Date;
+  userId: string;
+}
+
+export interface JoinConversationEvent extends EventBase {
+  action: "joinConversation";
+}
+
+export interface LeaveConversationEvent extends EventBase {
+  action: "leaveConversation";
+}
+
+export interface SendMessageEvent extends EventBase {
+  action: "sendMessage";
+  text: string;
+}
+
+export interface SendDirectMessageEvent extends EventBase {
+  action: "sendDirectMessage";
+  text: string;
+  recipientId: string;
+}
+
+export interface SendAttachementEvent extends EventBase {
+  action: "sendAttachement";
+  text: string;
+  attachmentUri: string;
+}
